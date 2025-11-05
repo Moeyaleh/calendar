f24d20704b552aca781e1f24b1e6d706e2fda5cc
Moeyaleh <Fakhrimayyaleh@gmail.com>
2025-11-05 21:30:52 +0200

    added some UI with test samples

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="everything-container">
+        <div className="tasks-container">
+          <div className="sidebar-container">
+            <div className="brandName-container">
+              <p className="brandName">TASKER</p>
+            </div>
+            <div className="listOfTasks">
+              <ul>
+                <li>Play Valorant</li>
+                <li>Study</li>
+                <li>Program</li>
+                <li>Code</li>
+              </ul>
+            </div>
+            <div className="quote">
+              <h1>
+                {" "}
+                "Start where you are. Use what you have. Do what you can." -
+                moeyaleh
+              </h1>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
